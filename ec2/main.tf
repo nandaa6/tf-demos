d1385017ca2d5ebd8aaa43b7095b31facf4957e5
nandaa6 <bnandakumar29@gmail.com>
2025-05-29 05:23:57 +0530

    updated the new

---
+++ ec2/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "instance1" {
+  ami           = "ami-08b5b3a93ed654d19"
+  instance_type = "t2.micro"
+  tags = {
+    name = "sample-terraform-instance"
+  }
+}
