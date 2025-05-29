1a868dfbb6ae136e549aef3c33049cdfc50ebb41
nandaa6 <bnandakumar29@gmail.com>
2025-05-29 10:35:29 +0530

    new backend

--- ec2/main.tf
+++ ec2/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "instance1" {
   ami           = "ami-08b5b3a93ed654d19"
   instance_type = "t2.micro"
   tags = {
-    name = "sample2-terraform-instance-branch2"
+    name = "sample2-terraform-instance-main"
   }
 }
 
