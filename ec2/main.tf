a35a62dbfcab01b881acad82ea0dab8b02cd4248
nandaa6 <learnnanda6@gmail.com>
2025-05-29 08:32:26 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "instance1" {
   ami           = "ami-08b5b3a93ed654d19"
   instance_type = "t2.micro"
   tags = {
-    Name = "sample2-terraform-instance"
+    name = "sample2-terraform-instance"
   }
 }
