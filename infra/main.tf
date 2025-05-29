1979ff72dc5a3206174a3ccf1b4a8bec4b4de66f
nandaa6 <bnandakumar29@gmail.com>
2025-05-29 21:07:39 +0530

    new linne

--- infra/main.tf
+++ infra/main.tf
@@ -20,7 +20,7 @@ provider "aws" {
 }
 
 module "network" {
-  source = "./modules/network"
+  source = "./infra/modules/network"
   name   = var.project_name
   cidr   = "10.0.0.0/16"
   azs    = ["us-east-1a", "us-east-1b"]
